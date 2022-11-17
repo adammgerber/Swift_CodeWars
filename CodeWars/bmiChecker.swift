659df5ad4e1c3df4f9ab6b092eb6dbf0daca7219
Adam Milo <amilo@ultralig.ht>
2022-11-17 06:32:33 -0500

    firstNotconsecutive

---
+++ CodeWars/bmiChecker.swift
@@ -0,0 +1,8 @@
+//
+//  bmiChecker.swift
+//  CodeWars
+//
+//  Created by Adam Gerber on 17/11/2022.
+//
+
+import Foundation
--- CodeWars/firstNotConsecutive.swift
+++ CodeWars/firstNotConsecutive.swift
@@ -13,7 +13,7 @@
 
 import Foundation
 
-func firstNonConsecutive (_ arr: [Int]) -> Int? {
+func firstNonConsecutive (_ arr: [Int]) -> Int?  {
   for i in 1..<arr.count {
     if arr[i] - arr[i-1] > 1 { return arr[i] }
   }
