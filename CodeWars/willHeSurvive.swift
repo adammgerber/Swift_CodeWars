0ab339a9bc1949f32740624f5effaf90be3d3f9d
Adam Milo <amilo@ultralig.ht>
2022-11-17 07:11:29 -0500

    willHeSurvive

---
+++ CodeWars/willHeSurvive.swift
@@ -0,0 +1,16 @@
+//
+//  willHeSurvive.swift
+//  CodeWars
+//
+//  Created by Adam Gerber on 17/11/2022.
+//
+
+//A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+
+//Return True if yes, False otherwise
+
+import Foundation
+
+func hero(bullets: Int, dragons: Int) -> Bool {
+    return bullets >= dragons * 2
+}
