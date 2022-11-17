fb68de419dce8e45e492df2b0c8c63dd4c0e24ef
Adam Milo <amilo@ultralig.ht>
2022-11-17 06:33:00 -0500

    bmiChecker

--- CodeWars/bmiChecker.swift
+++ CodeWars/bmiChecker.swift
@@ -5,4 +5,23 @@
 //  Created by Adam Gerber on 17/11/2022.
 //
 
+
+//Write function bmi that calculates body mass index (bmi = weight / height2).
+
+//if bmi <= 18.5 return "Underweight"
+
+//if bmi <= 25.0 return "Normal"
+
+//if bmi <= 30.0 return "Overweight"
+
+//if bmi > 30 return "Obese"
+
 import Foundation
+
+func bmi(_ weight: Int, _ height: Double) -> String {
+    let index = Double(weight) / (height * height)
+    if index <= 18.5 {return "Underweight"}
+    if index <= 25.0 {return "Normal"}
+    if index <= 30.0 {return "Overweight"}
+    return "Obese"
+}
